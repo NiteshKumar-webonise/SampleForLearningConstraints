63b437e7b72d4ba5696a5eb354cbc2b3c2c9ab4f
NiteshKumar-webonise <nitesh.pal@weboniselab.com>
2016-09-25 23:04:37 +0530

    added samples for learning constraints

---
+++ AutolayoutSampleTips/AutolayoutSampleTips/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AutolayoutSampleTips
+//
+//  Created by Nitesh Kumar Pal on 17/02/15.
+//  Copyright (c) 2015 Nitesh Kumar Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var layoutContsriantLeading: NSLayoutConstraint!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // layoutContsriantLeading.constant = 100
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
